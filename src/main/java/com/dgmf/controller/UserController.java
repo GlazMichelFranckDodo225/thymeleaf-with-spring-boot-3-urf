306337083666c95bf0aae4be888ceb07345b79ec
GlazMichelFranckDodo225 <cda.gmfd@gmail.com>
2024-01-19 07:32:18 +0100

    Thymeleaf "th:text" Attribute And Variable Expressions "${}"

---
+++ src/main/java/com/dgmf/controller/UserController.java
@@ -0,0 +1,26 @@
+package com.dgmf.controller;
+
+import com.dgmf.entity.User;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class UserController {
+    // Handle Method for "variable-expression" Request
+    // http://localhost:8080/variable-expression
+    @GetMapping("variable-expression")
+    public String variableExpression(Model model) {
+        User user = User.builder()
+                .name("John Doe")
+                .email("johndoe@gmail.com")
+                .role("ADMIN")
+                .gender("Male")
+                .build();
+
+        model.addAttribute("user", user);
+
+        return "variable-expression";
+    }
+
+}
